2d245c72918415ce775c66d0262689a08e2aea45
achmadnukmanjaza <achmadnukmanjaza098@gmail.com>
2023-10-15 12:36:21 +0700

    tambah penjelasan untuk semua controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,28 +10,35 @@ class LoginController extends Controller
 {
     public function login()
     {
+        // Periksa apakah pengguna sudah terotentikasi. Jika iya, arahkan ke halaman "home".
         if (Auth::check()) {
             return redirect('home');
-        }else{
+        } else {
+            // Jika belum terotentikasi, tampilkan halaman "login".
             return view('login');
         }
     }
 
     public function actionlogin(Request $request)
     {
+        // Ambil alamat email dan kata sandi dari permintaan yang diterima.
         $userPass = [
             'email' => $request->input('email'),
             'password' => $request->input('password'),
         ];
 
-        if (Auth::Attempt($userPass)) {
+        // Coba untuk mengotentikasi pengguna dengan alamat email dan kata sandi yang diberikan.
+        if (Auth::attempt($userPass)) {
+            // Jika otentikasi berhasil, periksa peran pengguna.
             if (Auth::user()->role_id == 1) {
+                // Jika peran pengguna adalah 1, arahkan ke halaman "/pegawai".
                 return redirect('/pegawai');
             } else {
+                // Jika peran pengguna bukan 1, arahkan ke halaman "/absensi".
                 return redirect('/absensi');
             }
-            
-        }else{
+        } else {
+            // Jika otentikasi gagal, tampilkan pesan kesalahan dan arahkan kembali ke halaman awal.
             Session::flash('error', 'Email atau Password Salah');
             return redirect('/');
         }
@@ -39,6 +46,7 @@ class LoginController extends Controller
 
     public function actionlogout()
     {
+        // Logout pengguna dan arahkan kembali ke halaman awal (biasanya halaman login).
         Auth::logout();
         return redirect('/');
     }
--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,6 +11,7 @@ class PegawaiController extends Controller
 {
     public function pegawai()
     {
+        // Mengambil data pegawai dan peran terkait
         $data = User::join('roles', 'roles.id', '=', 'users.role_id')
                     ->get(['users.name as nama_pegawai', 'users.email as email_pegawai', 'roles.nama_role as role']);
         return view('pegawai')->with('data', $data);
@@ -18,6 +19,7 @@ class PegawaiController extends Controller
 
     public function role()
     {
+        // Mengambil semua data peran
         $data = Role::get();
         return view('role')->with('data', $data);
     }
@@ -25,9 +27,11 @@ class PegawaiController extends Controller
     public function absensi()
     {
         if (Auth::user()->role_id == 1) {
+            // Jika pengguna memiliki peran dengan ID 1 (misalnya, peran admin), ambil semua data absensi pegawai
             $data = Absensi::join('users', 'users.id', '=', 'absensis.user_id')
                         ->get(['users.name as nama_pegawai', 'absensis.absen_masuk as absen_masuk', 'absensis.absen_keluar as absen_keluar', 'absensis.keterangan as keterangan_absen']);
         } else {
+            // Jika pengguna tidak memiliki peran dengan ID 1, ambil hanya data absensi pengguna saat ini
             $data = Absensi::join('users', 'users.id', '=', 'absensis.user_id')
                         ->where('absensis.user_id', Auth::user()->id)
                         ->get(['users.name as nama_pegawai', 'absensis.absen_masuk as absen_masuk', 'absensis.absen_keluar as absen_keluar', 'absensis.keterangan as keterangan_absen']);
